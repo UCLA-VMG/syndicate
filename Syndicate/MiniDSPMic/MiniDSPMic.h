349202c508001ea5a6069d247d20d84f92ac42f2
Anirudh0707 <yani.bh@gmail.com>
2022-05-25 20:15:01 -0700

    Working audio struct

--- Syndicate/MiniDSPMic/MiniDSPMic.h
+++ Syndicate/MiniDSPMic/MiniDSPMic.h
@@ -35,7 +35,7 @@ struct MiniDSPMic : public Sensor
     
 };
 
-int _callback(const void* input_buffer, void* output_buffer,
+static int _callback(const void* input_buffer, void* output_buffer,
     unsigned long frames_per_buffer, const PaStreamCallbackTimeInfo* time_info,
     PaStreamCallbackFlags status_flags, void* userData);
 
