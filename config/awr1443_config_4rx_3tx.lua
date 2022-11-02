07db4ab54082c8c3bfc9f3d0251d340d899a03e7
adnan-armouti <adnan.armouti@gmail.com>
2022-11-01 19:34:17 -0700

    latest working version of syndicate, with FPGA radar reset before each trial

--- config/awr1443_config_4rx_3tx.lua
+++ config/awr1443_config_4rx_3tx.lua
@@ -143,7 +143,8 @@ ar1.ChirpConfig(1, 1, 0, 0, 0, 0, 0, 0, 1, 0)
 ar1.ChirpConfig(2, 2, 0, 0, 0, 0, 0, 0, 0, 1)
 -- ar1.FrameConfig(START_CHIRP_TX, END_CHIRP_TX, NUM_FRAMES, CHIRP_LOOPS, PERIODICITY, 0, 0, 1)
 -- ar1.FrameConfig(0, 2, 0, 128, 1000, 0, 0, 1)
-ar1.FrameConfig(0, 2, 0, 1, 31, 0, 0, 2)
+-- In the line below, set the last int to be 1 for software triggering and 2 for hardware triggering
+ar1.FrameConfig(0, 2, 0, 1, 31, 0, 0, 1)
 --  ar1.FrameConfig(0, 0, 0, 1, 999, 0, 0, 2)
 -- ar1.FrameConfig(0, 2, 0, 128, 999, 0, 0, 2)
 -------------------------------------
--- scripts/main.cpp
+++ scripts/main.cpp
@@ -14,9 +14,17 @@
 using boost::property_tree::ptree;
 
 int main(int argc, char *argv[]) {
+    std::string tree_filename;
+    if (argc > 1){
+        tree_filename = std::string(argv[1]);
+    }
+    else {
+        tree_filename = "C:/Users/Adnan/Documents/Github/syndicate/scripts/sensor_config_all.xml";
+    }
+
     // Extract XML pTree
     ptree tree;
-    std::string XML_PATH("C:/Users/Adnan/Documents/Github/syndicate/scripts/sensor_config.xml");
+    std::string XML_PATH(tree_filename);
     read_xml(XML_PATH, tree);
     ptree & ptree_global = tree.get_child("sensor_stack").get_child("global");
 
@@ -26,20 +34,33 @@ int main(int argc, char *argv[]) {
         rootPath += '/';
     // Create the main folder
     std::filesystem::create_directory(rootPath);
+    // Initial wait time
+    std::cout << "Waiting Before Acquisition"  << "!" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(ptree_global.get<int>("wait_time_init")));
+    std::cout << "Waiting Done!" << std::endl;
     // Iterate over the different trials
     for (int i = 0; i < ptree_global.get<int>("num_trials"); i++) {
         std::string trial_id = std::to_string(i);
+        
+        // Reset the FPGA
+        ptree::value_type global_config(std::pair("global", ptree_global));
+        ptree::value_type sensor_config(std::pair("sensor", tree.get_child("sensor_stack").get_child("reset_sensor")));
+        SerialPort reset_arduino(sensor_config, global_config);
+        std::cout << "Beginning Reset Process!" << std::endl;
+        reset_arduino.resetFPGA();
+        std::cout << "Completed Reset Process!" << std::endl;
+        
+        // Wait before each trial
         std::cout << "Waiting Before Trial " << trial_id << "!" << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(ptree_global.get<int>("wait_time_" + trial_id)));
         std::cout << "Waiting Done!" << std::endl;
         ptree_global.put<std::string>("save_path", rootPath + "trial_" + trial_id + '/');
         
-        std::cout << "Saving to " <<  tree.get_child("sensor_stack").get_child("global").get<std::string>("save_path") << std::endl;
-        
         // Create the trial folders
+        std::cout << "Saving to " <<  tree.get_child("sensor_stack").get_child("global").get<std::string>("save_path") << std::endl;
         std::filesystem::create_directory(ptree_global.get<std::string>("save_path"));
 
-        // // Initialize Sensor Stack
+        // Initialize Sensor Stack
         std::cout << std::endl;
         std::cout << "Initializing Sensor Stack" << std::endl;
         SensorStack mainStack(tree);
@@ -48,13 +69,5 @@ int main(int argc, char *argv[]) {
         std::cout << std::endl;
         std::cout << "Beginning Acquisition Process Now!" << std::endl;
         mainStack.Acquire(); // 10,800 [3 hrs] & 14,400 [4 hrs] & 21,600 [6 hrs]
-        
-        // Reset the FPGA
-        ptree::value_type global_config(std::pair("global", ptree_global));
-        ptree::value_type sensor_config(std::pair("sensor", tree.get_child("sensor_stack").get_child("reset_sensor")));
-        SerialPort reset_arduino(sensor_config, global_config);
-        std::cout << "Beginning Reset Process!" << std::endl;
-        reset_arduino.resetFPGA();
-        std::cout << "Ended Reset Process!" << std::endl;
     }
 }
