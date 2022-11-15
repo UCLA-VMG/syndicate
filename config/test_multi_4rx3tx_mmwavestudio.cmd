273321342549c454590f9a463a6263fccc648566
adnan-armouti <adnan.armouti@gmail.com>
2022-11-14 17:22:31 -0800

    working syndicate version for osa data collection

---
+++ config/test_multi_4rx3tx_mmwavestudio.cmd
@@ -0,0 +1,2 @@
+cd "C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\RunTime"
+cmd /C "C:\ti\mmwave_studio_02_01_01_00\mmWaveStudio\RunTime\mmWaveStudio.exe /lua C:\Users\Adnan\Documents\Github\syndicate\config\test_awr1443_config_4rx_3tx.lua"
